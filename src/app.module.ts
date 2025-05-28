724882dc02edbe7fa7ed612eb176167e3b3c93b5
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2025-05-28 11:05:10 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { DatabaseModule } from './database/database.module';
+import { EmailParserModule } from './email-parser/email-parser.module';
+import { EmailParserService } from './email-parser/email-parser.service';
+import { DriveModule } from './drive/drive.module';
+import { GmailModule } from './gmail/gmail.module';
+import { GmailController } from './gmail/gmail.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+
+@Module({
+  imports: [AuthModule, GmailModule, DriveModule, EmailParserModule, DatabaseModule],
+  controllers: [AppController, AuthController, GmailController],
+  providers: [AppService, EmailParserService],
+})
+export class AppModule {}
